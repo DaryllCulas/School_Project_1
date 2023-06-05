b1efd91a598d98dc0977021d58d98af3f0d2032e
DaryllCulas <culasdaryll99@gmail.com>
2023-06-06 01:30:25 +0800

    Add serverConfig.js

---
+++ nodejs-backend/serverConfig.js
@@ -0,0 +1,13 @@
+// Server Config
+const config = {
+  user: "sa",
+  password: "M@kar0v99",
+  server: "localhost",
+  database: "Cvsu_ITS",
+  port: 1433,
+  options: {
+    trustServerCertificate: true,
+  },
+};
+
+module.exports = config;
