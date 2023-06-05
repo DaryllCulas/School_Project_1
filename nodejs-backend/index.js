185f85f22c8732c392666cc1824558b65bdec7e7
DaryllCulas <culasdaryll99@gmail.com>
2023-06-06 02:09:14 +0800

    Modified: index.js => loginEndpoint not done

--- nodejs-backend/index.js
+++ nodejs-backend/index.js
@@ -1,28 +1,29 @@
 const express = require("express");
 const app = express();
-const serverConfiguration = require("./serverConfiguration");
-// const loginEndpoint = require("./loginEndpoint");
+const serverConfig = require("./serverConfig");
+const loginEndpoint = require("./loginEndpoint");
 // const logoutEndpoint = require("./logoutEndpoint");
+const path = require("path");
 const port = 5000;
 
 // Middleware for serving static assets
-app.use("/assets", express.static("assets"));
+app.use("/assets", express.static(path.join(__dirname, "../assets")));
 
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// // Redirect to loginForm
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/loginForm.html");
-// });
+// Redirect to loginForm
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Login-form/studentLoginForm.html"));
+});
 
 // // Dashboard endpoint
 // app.get("/dashboard", function (req, res) {
 //   res.sendFile(__dirname + "/dashboard.html");
 // });
 
-// app.use("/", loginEndpoint);
+app.use("/", loginEndpoint);
 // app.use("/", logoutEndpoint);
 
 app.listen(port, function () {
