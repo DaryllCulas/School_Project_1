90ea85c77e6cb10670bc770332c5b206f5c2defc
DaryllCulas <culasdaryll99@gmail.com>
2023-06-07 04:10:39 +0800

    Modified: Middleware for assets, Portals, & Login-Form done => next adminLogin

--- nodejs-backend/index.js
+++ nodejs-backend/index.js
@@ -9,19 +9,22 @@ const port = 5000;
 // Middleware for serving static assets
 app.use("/assets", express.static(path.join(__dirname, "../assets")));
 
+// Middleware for serveing static Portals
+app.use("/Portals", express.static(path.join(__dirname, "../Portals")));
+
+// Middleware for serving static studentLoginForm
+app.use("/Login-form", express.static(path.join(__dirname, "../Login-form")));
+
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//Redirect to loginPortal
+//Redirect to Homepage
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../index.html"));
 });
 
-// Redirect to loginForm
-// app.get("/student_login", (req, res) => {
-//   res.sendFile(path.join(__dirname, "../Login-form/studentLoginForm.html"));
-// });
+
 
 // Dashboard endpoint
 app.get("/dashboard", function (req, res) {
