da4bf2187fcba866634cfe30ddad09e21d0537f0
DaryllCulas <culasdaryll99@gmail.com>
2023-06-10 01:48:58 +0800

    Add loginEndpoint for adminLogin

--- nodejs-backend/loginEndpoint.js
+++ nodejs-backend/loginEndpoint.js
@@ -3,7 +3,7 @@ const mssql = require("mssql");
 const router = express.Router();
 const config = require("./serverConfiguration");
 
-// Login endpoint
+// Login endpoint for student login
 router.post('/student_login', (req, res) => {
   const { studentID, studentEmail, studentPassword } = req.body;
 
@@ -42,6 +42,49 @@ router.post('/student_login', (req, res) => {
     });
   });
 });
+
+
+//Login Endpoint for Admin Login
+router.post('/admin_login',(req, res) => {
+  const { facultyID, facultyEmail, facultyPassword } = req.body;
+
+  const pool = new mssql.ConnectionPool(config);
+  pool.connect((err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Server error');
+    }
+
+    const request = new mssql.Request(pool);
+    const query = `SELECT * FROM AdminFaculty WHERE facultyID = '${facultyID}' AND facultyEmail = '${facultyEmail}' AND facultyPassword = '${facultyPassword}'`;
+
+    request.query(query, (err, recordset) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send('Server error');
+      }
+
+      if (recordset.recordset.length > 0) {
+        res.send(`
+        <script>
+        alert("Login Successfully Login");
+        window.location.href = "../dashboard";
+          </script>
+          `);
+          
+      } else {
+        res.send(`
+        <script>
+          alert("Invalid data");
+          window.location.href = "/?error=1";
+        </script>
+      `);
+      }
+    });
+  });
+
+});
+
 
 module.exports = router;
 
