72fbeb0e54f127630731b854468ea3cc4121a3f2
DaryllCulas <culasdaryll99@gmail.com>
2023-06-12 05:43:19 +0800

    Modified: ITstudent table => add column for yearlevel and section

--- sql_database/Cvsu_ITSDBS.sql
+++ sql_database/Cvsu_ITSDBS.sql
@@ -1,70 +1,80 @@
 CREATE DATABASE Cvsu_ITS;
 
+/* IT student Table */
 CREATE TABLE ITstudent (
     studentID INT IDENTITY(2023001,1) PRIMARY KEY,
     studentFirstName VARCHAR(255) NOT NULL,
     studentLastName VARCHAR(255) NOT NULL,
-    studentEmail VARCHAR(255) NOT NULL,
+    YearLevelAndSection VARCHAR(255) NOT NULL,
+    studentEmail VARCHAR(255) NOT NULL,     
     studentPassword VARCHAR(255) NOT NULL
 );
 
+SELECT * FROM ITstudent;
+
 INSERT INTO ITstudent(
-     studentFirstName,studentLastName, 
-     studentEmail,studentPassword
+     studentFirstName,studentLastName,
+     YearLevelAndSection,studentEmail,
+     studentPassword
 )VALUES(
     'Daryll','Culas', 
-    'coolastthereal99@gmail.com',
+    'BSIT 3-3','coolastthereal99@gmail.com',
     'Iamdaryll9'
 );
 
 INSERT INTO ITstudent(
-     studentFirstName,studentLastName, 
-     studentEmail,studentPassword
+     studentFirstName,studentLastName,
+     YearLevelAndSection,studentEmail,
+     studentPassword
 )VALUES(
     'Tanjiro','Kamado', 
-    'tanjirokamado01@gmail.com',
+    'BSIT 2-1','tanjirokamado01@gmail.com',
     'demonslayer01'
 );
 
 INSERT INTO ITstudent(
-     studentFirstName,studentLastName, 
-     studentEmail,studentPassword
+     studentFirstName,studentLastName,
+     YearLevelAndSection,studentEmail,
+     studentPassword
 )VALUES(
     'Nezuko','Kamado', 
-    'nezukokamado01@gmail.com',
+    'BSIT 2-4','nezukokamado01@gmail.com',
     'demonslayer02'
 );
 
 INSERT INTO ITstudent(
-     studentFirstName,studentLastName, 
-     studentEmail,studentPassword
+     studentFirstName,studentLastName,
+     YearLevelAndSection,studentEmail,
+     studentPassword
 )VALUES(
     'giyuu','tomioka', 
-    'giyutomioka03@gmail.com',
+    'BSIT 1-5','giyutomioka03@gmail.com',
     'demonslayer03'
 );
 
-
 INSERT INTO ITstudent(
-     studentFirstName,studentLastName, 
-     studentEmail,studentPassword
+     studentFirstName,studentLastName,
+     YearLevelAndSection,studentEmail,
+     studentPassword
 )VALUES(
     'Tengen','Uzui', 
-    'tengenuzui11@gmail.com',
+    'BSIT 4-1','tengenuzui11@gmail.com',
     'demonslayer04'
 );
 
 
 INSERT INTO ITstudent(
-     studentFirstName,studentLastName, 
-     studentEmail,studentPassword
+     studentFirstName,studentLastName,
+     YearLevelAndSection,studentEmail,
+     studentPassword
 )VALUES(
     'Tiburcio','Kanor', 
-    'tiburciokanor69@gmail.com',
+    'BSIT 3-2','tiburciokanor69@gmail.com',
     'demonslayer05'
 );
 
-SELECT * FROM ITstudent;
+
+/* Admin Faculty Table */
 
 CREATE TABLE AdminFaculty(
     facultyID INT IDENTITY(2023001,5) PRIMARY KEY,
