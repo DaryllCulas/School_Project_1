0020b49c14ad9a19ad7acb835689eca9e07d6e36
DaryllCulas <culasdaryll99@gmail.com>
2023-06-12 23:32:09 +0800

    Modified: /logout => from alert() to confirm()

--- nodejs-backend/logoutEndpoint.js
+++ nodejs-backend/logoutEndpoint.js
@@ -4,8 +4,13 @@ const router = express.Router();
 // Logout Endpoint 
 router.get("/logout", (req, res) => {
   res.send(`<script> 
-  alert('Logging out...'); 
-  window.location.href="../Portals/portal.html";
+  if(confirm('Are you sure you want to log out?')) {
+    alert('Logging out...');
+    window.location.href = "../Portals/portal.html";
+  }
+  else {
+    history.back();
+  }
   </script>`);
 });
 
