419d52eb855f2c8b3cc3ed5a1f526e22ca6bde90
DaryllCulas <culasdaryll99@gmail.com>
2023-06-14 20:51:37 +0800

    Modified: /logout => add session_destroy

--- nodejs-backend/logoutEndpoint.js
+++ nodejs-backend/logoutEndpoint.js
@@ -4,15 +4,17 @@ const router = express.Router();
 // Logout Endpoint 
 router.get("/logout", (req, res) => {
   res.send(`<script> 
-  if(confirm('Are you sure you want to log out?')) {
-    alert('Logging out...');
-    window.location.href = "../Portals/portal.html";
-  }
-  else {
-    history.back();
-  }
+    if (confirm('Are you sure you want to log out?')) {
+      alert('Logging out...');
+      fetch('/logout', { method: 'POST' })
+        .then(() => {
+          window.location.href = "../Portals/portal.html";
+        })
+        .catch((error) => console.log(error));
+    } else {
+      history.back();
+    }
   </script>`);
 });
-
 
 module.exports = router;
