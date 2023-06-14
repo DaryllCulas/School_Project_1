5d47b500c18d50435a648d5dd242e9a9d302d4ad
DaryllCulas <culasdaryll99@gmail.com>
2023-06-14 19:47:48 +0800

    add api endpoint fetch recordsets, session management, and fetching session id (admin data)

--- nodejs-backend/loginEndpoint.js
+++ nodejs-backend/loginEndpoint.js
@@ -2,6 +2,18 @@ const express = require("express");
 const mssql = require("mssql");
 const router = express.Router();
 const config = require("./serverConfiguration");
+const session = require("express-session");
+const path = require("path");
+
+// Configure express-session middleware
+router.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: false,
+    // You can customize the session configuration as per your needs
+  })
+);
 
 // Login endpoint for student login
 router.post('/student_login', (req, res) => {
@@ -24,12 +36,15 @@ router.post('/student_login', (req, res) => {
       }
 
       if (recordset.recordset.length > 0) {
+        // Store student's credentials in the session
+        req.session.student = recordset.recordset[0];
+
         res.send(`
         <script>
-        alert("Login Successfully!");
-        window.location.href = "${'/studentDashboard'}";
-          </script>
-          `);
+          alert("Login Successfully!");
+          window.location.href = "${'/studentDashboard'}";
+        </script>
+        `);
           
       } else {
         res.send(`
@@ -37,14 +52,13 @@ router.post('/student_login', (req, res) => {
           alert("Invalid data");
           window.location.href = "../Login-form/studentLoginForm.html";
         </script>
-      `);
+        `);
       }
     });
   });
 });
 
-
-//Login Endpoint for Admin Login
+// Login Endpoint for Admin Login
 router.post('/admin_login',(req, res) => {
   const { facultyID, facultyEmail, facultyPassword } = req.body;
 
@@ -65,12 +79,15 @@ router.post('/admin_login',(req, res) => {
       }
 
       if (recordset.recordset.length > 0) {
+        // Store admin's credentials in the session
+        req.session.admin = recordset.recordset[0];
+
         res.send(`
         <script>
-        alert("Login Successfully");
-        window.location.href = "${'/adminDashboard'}";
-          </script>
-          `);
+          alert("Login Successfully");
+          window.location.href = "${'/adminDashboard'}";
+        </script>
+        `);
           
       } else {
         res.send(`
@@ -78,11 +95,42 @@ router.post('/admin_login',(req, res) => {
           alert("Invalid data");
           window.location.href = "../Login-form/adminLoginForm.html";
         </script>
-      `);
+        `);
       }
     });
   });
+});
 
+// Endpoint for fetching admin profile data
+router.get('/admin_profile_data', (req, res) => {
+  if (!req.session.admin) {
+    return res.status(401).send('Unauthorized');
+  }
+
+  const pool = new mssql.ConnectionPool(config);
+  pool.connect((err) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Server error');
+    }
+
+    const request = new mssql.Request(pool);
+    const facultyID = req.session.admin.facultyID;
+    const query = `SELECT * FROM AdminFaculty WHERE facultyID = '${facultyID}'`;
+
+    request.query(query, (err, recordset) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send('Server error');
+      }
+
+      if (recordset.recordset.length === 0) {
+        return res.status(404).send('Admin not found');
+      }
+
+      res.json(recordset.recordset);
+    });
+  });
 });
 
 
@@ -110,4 +158,3 @@ router.get('/recordsets', (req, res) => {
 });
 
 module.exports = router;
-
